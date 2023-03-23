26cb03b13815330c7ff2629478c743f6425a9e91
danyelleangelo <danyangelofsa@gmail.com>
2023-03-22 22:34:17 -0300

    Remove lib of mock, and add math librarys in test

--- DC3/dc3_test.cpp
+++ DC3/dc3_test.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
-#include "gmock/gmock.h"
+//#include "gmock/gmock.h"
 #include <cstdlib>
 #include <iostream>
+#include <math.h>
 #include "dc3.hpp"
 
 using namespace std;
