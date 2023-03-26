a2d1f34e9c794ef9537f01784139723149f19698
danyelleangelo <danyangelofsa@gmail.com>
2023-03-26 14:58:37 -0300

    generate large text: passing number of permutations by command line

--- DC3/generate_text.cpp
+++ DC3/generate_text.cpp
@@ -6,19 +6,20 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    if(argc < 3) {
+    if(argc < 4) {
         cout << "The is parameters is missing. Parameters requireds: \n1- word that is permuted; \n2- File for write text." << endl;
         exit(EXIT_FAILURE);
     }
 
     ofstream file;
-    file.open(argv[2],ios::trunc);
+    file.open(argv[3],ios::trunc);
 
     string word = string(argv[1]);
     int n = word.size(), i=0, l,r;
-    
+    int nPermutations = atoi(argv[2]);
+
     file.write(&word[0], n*sizeof(char));
-    while(i < 40) {
+    while(i < nPermutations) {
         r = 2+ rand() % (n-1);
         l = rand() % (r-1);
         file.write(&word[l], (r-l)*sizeof(char));
