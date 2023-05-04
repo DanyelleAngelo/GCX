df00e82c4798ffe2cfc83aa179ae081683d76fb2
Danyelle Angelo <danyangelofsa@gmail.com>
2023-05-04 09:22:01 -0300

    splitting output files

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include "radix.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if(argc < 4) {
+        cout << "\n >> Number of invalid arguments! << \n" <<
+                "\tList of arguments:\n" <<
+                "\t\tInput file  for encode or decode\n" <<
+                "\t\tOutput file, contains the result of the chosen operation)\n" <<
+                "\t\tOperation - for encode choose: \"e\", for decode choose: \"d\" " << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    char op = argv[3][0];
+    grammar(argv[1], argv[2], op);
+}
