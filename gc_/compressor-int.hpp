8ea03ccb508c8691c35a4962516b26274b4dbf17
Danyelle Angelo <danyangelofsa@gmail.com>
2023-08-02 01:17:10 -0300

    Refactor: improvement of methods, removal of redundancies, adjustments of macros and so on.

--- gc_/compressor-int.hpp
+++ gc_/compressor-int.hpp
@@ -5,15 +5,51 @@
 #include <vector>
 
 using namespace std;
+
 /**
  * @brief Identifica a operação escolhida pelo usuário, e chama o método responsável pela operação (encode ou decode).
  * 
- * @param fileIn contém o texto plano (caso a opção escolhida seja encode) ou o texto comprimido (caso a opção escolhida seja decode)
+ * @param fileIn nome do arquivo contendo o texto sobre o qual realizaremos nossas operações/
  * @param fileOut arquivo vazio, onde será gravado o resultado da operação escolhida
  * @param op armazena a opção escolhida pelo usuário "e" para encode, e "d" para decode
  * @param rulesSize tamanho das regras que devemos gerar
  */
-void grammar(char *fileIn, char *fileOut, char op, int ruleSize);
+void grammarInteger(char *fileIn, char *fileOut, char op, int ruleSize);
+
+/**
+ * @brief Exibe na tela as informações da gramática
+ * 
+ * @param header armazena quantidade de níveis e número de regras por nível da gramática
+ * @param levels número de níveis
+ * @param mod tamanho das regras na gramática
+ */
+void grammarInfo(vector<uint32_t> header, int levels, int mod);
+
+/**
+ * @brief Realiza a leitura e armazena o texto codificado em uma variável.
+ * 
+ * @param fileName nome do arquivo contendo a gramática que comprime o texto
+ * @param uText responsável por armazenar o texto compactado ao final do método
+ * @param textSize armazena o tamanho tamanho do texto (excluindo as informaçoes da gramática,e as regras do último nível)
+ * @param charRules armazena as regras do último nível da recursão, ou seja as regras que são originadas do texto plano
+ * @param mod tamanho das tuplas do texto
+ * @param header contém as informações da gramática, quantidade de níveis, bem como quantidade de regras por nível
+ */
+void readCompressedFile(char *fileName, uint32_t *&uText, int32_t &textSize,  unsigned char* &charRules, int mod, vector<uint32_t> &header);
+
+/**
+ * @brief realiza a compactação do texto por meio de sucessivas chamadas recursivas, onde são geradas regras que codificam o texto.
+ * 
+ * @param text0 texto plano
+ * @param uText texto a ser reduzido, codificado através de um inteiro
+ * @param textSize tamanho do texto
+ * @param fileName nome do arquivo onde são gravadas as regras geradas a cada nível
+ * @param level nível atual
+ * @param mod tamanho das tuplas do texto
+ * @param header contém as informações da gramática, quantidade de níveis, quantidade de regras por nível
+ * @param sigma armazena o tamanho do alfabeto a ser usado nesse nível (é a quantidade de regras do nível anterior.)
+ */
+void encode(unsigned char *text0, uint32_t *uText, int32_t textSize, char *fileName, int level, int mod, vector<uint32_t> &header, uint32_t sigma);
 
 /**
  * @brief realiza a descompressão do texto, por meio da decodificação do texto nível a nível.
@@ -24,22 +60,44 @@ void grammar(char *fileIn, char *fileOut, char op, int ruleSize);
  * @param qtyLevels quantidade de níveis na gramática
  * @param fileName arquivo onde deve ser gravado o resultado da descompressão
  * @param rules0 armazena as regras do último nível da recursão, ou seja as regras que são originadas do texto plano
- * @param module tamanho das tuplas do texto
- * @param grammarInfo contém as informações da gramática, quantidade de níveis, quantidade de regras por nível
+ * @param mod tamanho das tuplas do texto
+ * @param header contém as informações da gramática, quantidade de níveis, quantidade de regras por nível
  */
-void decode(uint32_t *text, int32_t textSize, int level, int qtyLevels, char *fileName, unsigned char *rules0, int module, vector<uint32_t> &grammarInfo);
+void decode(uint32_t *text, int32_t textSize, int level, int qtyLevels, char *fileName, unsigned char *charRules, int mod, vector<uint32_t> &header);
 
 /**
- * @brief Decodifica o textp reduzido em determinado nível da gramática
+ * @brief Cria e abre o arquivo para gravar as informações da gramática, e em seguida grava o símbolo inicial.
  * 
- * @param text conteúdo compactado lido do arquivo de entrado
- * @param symbol símbolo a ser decodificado (ao final conterá o símbolo já decodficado)
- * @param xsSize tamanho do símbolo a ser decodificado (ao final, conterá o tamanho do símbolo já decodificado)
- * @param l nível atual
- * @param start índice onde as regras do nível "l" começam
- * @param module tamanho das tuplas do texto
+ * @param fileName nome do arquivo
+ * @param startSymbol símbolo inicial
+ * @param header contém as informações da gramática
  */
-void decodeSymbol(uint32_t* text, uint32_t *&symbol, int32_t &xsSize, int l, int start, int module);
+void storeStartSymbol(char *fileName, uint32_t *startSymbol, vector<uint32_t> &header);
+
+/**
+ * @brief Armazena regras geradas em cada nível
+ * 
+ * @param text0 contém as regras do último nível da recursão
+ * @param uText contém as regras dos demais níveis
+ * @param tuples array contendo os índices iniciais de cada tupla em ordem lexicográfica
+ * @param rank classificação de cada tupla
+ * @param nTuples quantidade de tuplas
+ * @param fileName arquivo onde as regras devem ser salvas
+ * @param level indica o nível em que as regras foram geradas.
+ */
+void storeRules(unsigned char *text0, uint32_t *uText, uint32_t *tuples, uint32_t *rank, int32_t nTuples, char *fileName, int mod, int level);
+
+/**
+ * @brief Decodifica o texto reduzido em determinado nível da gramática
+ * 
+ * @param uText conteúdo compactado lido do arquivo de entrado
+ * @param xs texto reduzido a ser decodificado, ao final conterá o símbolo já decodficado
+ * @param xsSize tamanho do símbolo a ser decodificado (ao final, conterá o tamanho do símbolo já decodificado)
+ * @param level nível atual
+ * @param start índice  em `uText` onde as regras do nível "level" começam
+ * @param mod tamanho das tuplas do texto
+ */
+void decodeSymbol(uint32_t* uText, uint32_t *&xs, int32_t &xsSize, int level, int32_t start, int mod);
 
 /**
  * @brief abre o arquivo de saída e grava o texto decodificado
@@ -47,9 +105,10 @@ void decodeSymbol(uint32_t* text, uint32_t *&symbol, int32_t &xsSize, int l, int
  * @param text texto decodificado
  * @param textSize tamanho do texto
  * @param fileName arquivo de saída
- * @param rules0 armazena as regras do último nível da recursão, ou seja as regras que são originadas do texto plano
- * @param module tamanho das tuplas do texto
+ * @param charRules regras do último nível da recursão
+ * @param mod tamanho das tuplas do texto
  */
-void saveDecodedText(uint32_t *symbol, int32_t textSize, char *fileName, unsigned char *rules0, int module);
+void saveDecodedText(uint32_t *xs, int32_t xsSize, char *fileName, unsigned char *charRules, int module);
+
 
 #endif
--- gc_/compressor.cpp
+++ gc_/compressor.cpp
@@ -6,79 +6,87 @@
 #include <sdsl/int_vector.hpp>
 #include <sdsl/coder.hpp>
 
+#define ASCII_SIZE 255
+
 using namespace std;
 
-int numberOfSentries(int32_t textSize, int module){
-    if(textSize > module && textSize % module != 0) {
-        return module - (textSize % module);
-    } else if(textSize % module !=0) {
-        return module - textSize;
+void isFileOpen(FILE * file, const char *msg) {
+    if(file != NULL) return;
+
+    cout << "\x1b[31m[ERROR]\x1b[0m " << msg << endl;
+    exit(EXIT_FAILURE);
+}
+
+int numberOfSentries(int32_t textSize, int mod){
+    if(textSize > mod && textSize % mod != 0) {
+        return mod - (textSize % mod);
+    } else if(textSize % mod !=0) {
+        return mod - textSize;
     }
     return 0;
 }
 
-void readPlainText(char *fileName, unsigned char *&text, int32_t &textSize, int module) {
+void readPlainText(char *fileName, unsigned char *&text, int32_t &textSize, int mod) {
     FILE*  file= fopen(fileName,"r");
-
-    if(file == NULL) {
-        cout << "An error occurred while opening the input plain file" << endl;
-        exit(EXIT_FAILURE);
-    }
+    isFileOpen(file, "An error occurred while opening the input plain file");
 
     fseek(file, 0, SEEK_END);
     textSize = ftell(file);
     int32_t i = textSize;
-    int nSentries=numberOfSentries(textSize, module);
+    int nSentries=numberOfSentries(textSize, mod);
     textSize += nSentries;
+
     text = (unsigned char*)malloc(textSize*sizeof(unsigned char));
     while(i < textSize) text[i++] =0;
+
     fseek(file, 0, SEEK_SET);
     fread(text, 1, textSize-nSentries, file);
     fclose(file);
 }
 
-void radixSort(uint32_t *uText, int tupleIndexSize, uint32_t *tupleIndex, long int level, int module){
-    uint32_t *tupleIndexTemp = (uint32_t*) calloc(tupleIndexSize, sizeof(uint32_t));
-    long int big=uText[0];
+void radixSort(uint32_t *uText, int32_t nTuples, uint32_t *tuples, uint32_t sigma, int mod){
+    uint32_t *tupleIndexTemp = (uint32_t*) calloc(nTuples, sizeof(uint32_t));
+    
+    for(int i=0, j=0; i < nTuples; i++, j+=mod)tuples[i] = j;
 
-    for(int i=1; i < tupleIndexSize*module;i++)if(uText[i] > big)big=uText[i];
-    for(int i=0, j=0; i < tupleIndexSize; i++, j+=module)tupleIndex[i] = j;
-    //Tentar entender qual deve ser o tamanho do alfabeto, definir este tamanho como sendo o maior valor em ordem lexicográfica está dando erro, sendo preciso incrementar
-    long int sigma = big+module;
-    int *bucket =(int*) calloc(sigma, sizeof(int));
+    sigma = (sigma < ASCII_SIZE + mod) ? ASCII_SIZE +mod : sigma+mod;
+    uint32_t *bucket =(uint32_t*) calloc(sigma, sizeof(uint32_t));
 
-    for(int d= module-1; d >=0; d--) {
-        for(int i=0; i < sigma;i++)bucket[i]=0;
-        for(int i=0; i < tupleIndexSize; i++) bucket[uText[tupleIndex[i] + d]+1]++; 
+
+    for(int d= mod-1; d >=0; d--) {
+        for(int i=0; i < sigma;i++)bucket[i]=0;//TODO
+        for(int i=0; i < nTuples; i++) bucket[uText[tuples[i] + d]+1]++; 
         for(int i=1; i < sigma; i++) bucket[i] += bucket[i-1];
 
-        for(int i=0; i < tupleIndexSize; i++) {
-            int index = bucket[uText[tupleIndex[i] + d]]++;
-            tupleIndexTemp[index] = tupleIndex[i];
+        for(int i=0; i < nTuples; i++) {
+            int index = bucket[uText[tuples[i] + d]]++;
+            tupleIndexTemp[index] = tuples[i];
         }
-        for(int i=0; i < tupleIndexSize; i++) tupleIndex[i] = tupleIndexTemp[i];
+        for(int i=0; i < nTuples; i++) tuples[i] = tupleIndexTemp[i];
     }
 
     free(bucket);
     free(tupleIndexTemp);
 }
 
-void createLexNames(uint32_t *uText, uint32_t *tupleIndex, uint32_t *rank, long int nTuples, int module, int32_t &qtyRules) {
-    qtyRules= 1;
-    rank[tupleIndex[0]/module] = 1;
+void createLexNames(uint32_t *uText, uint32_t *tuples, uint32_t *rank, int32_t &qtyRules, long int nTuples, int mod) {
+    int32_t name = 1;
+    rank[tuples[0]/mod] = name;
+
     for(int32_t i=1; i < nTuples; i++) {
         bool equal = true;
-        for(int j=0; j < module; j++)
-            if(uText[tupleIndex[i-1]+j] != uText[tupleIndex[i]+j]){
+        for(int j=0; j < mod; j++){
+            if(uText[tuples[i-1]+j] != uText[tuples[i]+j]){
                 equal = false;
                 break;
             }
-        if(equal)rank[tupleIndex[i]/module] = rank[tupleIndex[i-1]/module];
-        else {
-            rank[tupleIndex[i]/module] = rank[tupleIndex[i-1]/module] + 1;
-            qtyRules++;
         }
+
+        if(equal)rank[tuples[i]/mod] = name;
+        else rank[tuples[i]/mod] = ++name;
     }
+
+    qtyRules = name;
     #if DEBUG == 1
         printf("## Número de trincas %d, quantidade de trincas únicas: %d.\n", nTuples, qtyRules);
     #endif
