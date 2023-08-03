03490ef016b492a534d42381534bc3d8ffbb8abf
Danyelle Angelo <danyangelofsa@gmail.com>
2023-08-03 12:45:35 -0300

    moving scripts file

--- scripts/measure_compression.sh
+++ scripts/measure_compression.sh
@@ -16,6 +16,9 @@ fi
 if [ ! -d "$COMPRESSED_DIR" ]; then
     mkdir -p "$COMPRESSED_DIR"
 fi
+if [ ! -d "report" ]; then
+    mkdir -p "report"
+fi
 
 (
 echo -e "\n${GREEN}%%% Download files from a list, then descompress the files and remove the compressed files.${RESET}."
@@ -48,12 +51,19 @@ for plain_file in $FILE_PATHS; do
     in_plain="../$PIZZA_DIR/${file_name[1]}"
     out_compressed="../$COMPRESSED_DIR/${file_name[1]}-r3-int"
     out_descompressed=$out_compressed-plain
-    
+    file_report="report/compressed-rules3-encodedInteger.csv"
+
+    if [ -e "$file_report" ]; then
+        #para gerarmos um relatório novo
+        rm $file_report
+    fi
+
     echo -e "\n${BLUE}####### FILE: ${file_name[1]} ${RESET}"
     #compress
     make run_compressor MODE=e CODEC=int RULES=3 IN_PLAIN_TEXT_FILE=$in_plain COMPRESSED_FILE=$out_compressed -C compressor/
     #decompress
     make run_compressor MODE=d CODEC=int RULES=3 IN_PLAIN_TEXT_FILE=$in_plain COMPRESSED_FILE=$out_compressed OUT_PLAIN_TEXT_FILE=$out_descompressed -C compressor/
+    break
 done
 )
 
