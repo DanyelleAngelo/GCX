606a02463533d7a240b149dc7d4ae778225de287
Danyelle Angelo <danyangelofsa@gmail.com>
2023-08-02 23:27:11 -0300

    Adding a script to perform encoding with variable size

---
+++ compressor/abbrevs.h
@@ -0,0 +1,24 @@
+/**
+  \file abbrevs.h
+  \brief Type abbreviations for the lazy ones.
+  \internal g.p.telles.
+**/
+
+#ifndef ABBREVS_H
+#define ABBREVS_H
+
+#include <inttypes.h>
+
+typedef unsigned int uint;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#endif
