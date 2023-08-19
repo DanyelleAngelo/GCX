92cfc7ee2e723fcfb857bc75dda46db351a02024
Danyelle Angelo <danyangelofsa@gmail.com>
2023-08-19 15:57:03 -0300

    restored the deleted file

--- compressor.cpp
+++ compressor.cpp
@@ -15,23 +15,23 @@ void isFileOpen(FILE * file, const char *msg) {
     exit(EXIT_FAILURE);
 }
 
-int padding(int32_t textSize, int coverage){
-    if(textSize > coverage && textSize % coverage != 0) {
-        return coverage - (textSize % coverage);
-    } else if(textSize % coverage !=0) {
-        return coverage - textSize;
+int numberOfSentries(int32_t textSize, int mod){
+    if(textSize > mod && textSize % mod != 0) {
+        return mod - (textSize % mod);
+    } else if(textSize % mod !=0) {
+        return mod - textSize;
     }
     return 0;
 }
 
-void readPlainText(char *fileName, unsigned char *&text, int32_t &textSize, int coverage) {
+void readPlainText(char *fileName, unsigned char *&text, int32_t &textSize, int mod) {
     FILE*  file= fopen(fileName,"r");
     isFileOpen(file, "An error occurred while opening the input plain file");
 
     fseek(file, 0, SEEK_END);
     textSize = ftell(file);
     int32_t i = textSize;
-    int nSentries=padding(textSize, coverage);
+    int nSentries=numberOfSentries(textSize, mod);
     textSize += nSentries;
 
     text = (unsigned char*)malloc(textSize*sizeof(unsigned char));
@@ -42,16 +42,16 @@ void readPlainText(char *fileName, unsigned char *&text, int32_t &textSize, int 
     fclose(file);
 }
 
-void radixSort(uint32_t *uText, int32_t nTuples, uint32_t *tuples, uint32_t &sigma, int coverage){
+void radixSort(uint32_t *uText, int32_t nTuples, uint32_t *tuples, uint32_t sigma, int mod){
     uint32_t *tupleIndexTemp = (uint32_t*) calloc(nTuples, sizeof(uint32_t));
     
-    for(int i=0, j=0; i < nTuples; i++, j+=coverage)tuples[i] = j;
+    for(int i=0, j=0; i < nTuples; i++, j+=mod)tuples[i] = j;
 
-    sigma = (sigma < ASCII_SIZE + coverage) ? ASCII_SIZE + coverage : sigma+coverage;
+    sigma = (sigma < ASCII_SIZE + mod) ? ASCII_SIZE +mod : sigma+mod;
     uint32_t *bucket =(uint32_t*) calloc(sigma, sizeof(uint32_t));
 
 
-    for(int d= coverage-1; d >=0; d--) {
+    for(int d= mod-1; d >=0; d--) {
         for(int i=0; i < sigma;i++)bucket[i]=0;//TODO
         for(int i=0; i < nTuples; i++) bucket[uText[tuples[i] + d]+1]++; 
         for(int i=1; i < sigma; i++) bucket[i] += bucket[i-1];
@@ -67,21 +67,21 @@ void radixSort(uint32_t *uText, int32_t nTuples, uint32_t *tuples, uint32_t &sig
     free(tupleIndexTemp);
 }
 
-void createLexNames(uint32_t *uText, uint32_t *tuples, uint32_t *rank, int32_t &qtyRules, long int nTuples, int coverage) {
+void createLexNames(uint32_t *uText, uint32_t *tuples, uint32_t *rank, int32_t &qtyRules, long int nTuples, int mod) {
     int32_t name = 1;
-    rank[tuples[0]/coverage] = name;
+    rank[tuples[0]/mod] = name;
 
     for(int32_t i=1; i < nTuples; i++) {
         bool equal = true;
-        for(int j=0; j < coverage; j++){
+        for(int j=0; j < mod; j++){
             if(uText[tuples[i-1]+j] != uText[tuples[i]+j]){
                 equal = false;
                 break;
             }
         }
 
-        if(equal)rank[tuples[i]/coverage] = name;
-        else rank[tuples[i]/coverage] = ++name;
+        if(equal)rank[tuples[i]/mod] = name;
+        else rank[tuples[i]/mod] = ++name;
     }
 
     qtyRules = name;
