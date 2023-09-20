3f71997be484a83c538a5e47f3356cc8ef01cb23
Danyelle Angelo <danyangelofsa@gmail.com>
2023-09-20 23:25:47 +0300

    reports and logs

--- scripts/git_sync_script.sh
+++ scripts/git_sync_script.sh
@@ -2,7 +2,7 @@
 
 source measure_compression.sh
 
-report_execution=$REPORT_DIR/log-$CUR_DATE.txt
+report_execution=$REPORT_DIR/log-$CURR_DATE.txt
 
 check_and_create_folder | tee -a "$report_execution"
 download_files | tee -a "$report_execution"
