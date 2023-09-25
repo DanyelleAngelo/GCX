473bb482e19e26bb6964111feddedd1d36b1aaec
Danyelle Angelo <danyangelofsa@gmail.com>
2023-09-25 01:00:17 -0300

    adding call for extract execution and validation

--- scripts/git_sync_script.sh
+++ scripts/git_sync_script.sh
@@ -2,11 +2,13 @@
 
 source measure_compression.sh
 
-report_execution=$REPORT_DIR/log-$CURRENT_DATE.txt
+report_execution=$REPORT_DIR/log-$CUR_DATE.txt
 
 check_and_create_folder | tee -a "$report_execution"
 download_files | tee -a "$report_execution"
-dcx_generate_report | tee -a "$report_execution"
+compress_and_decompress_with_dcx | tee -a "$report_execution"
+valid_dcx_extract | tee -a "$report_execution"
+run_extract | tee -a "$report_execution"
 
 git add ../report/*
 git commit -m "reports and logs"
