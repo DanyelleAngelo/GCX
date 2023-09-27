a0983b10ca1aadc00c1c95ae9ecb517a183cd29f
Danyelle Angelo <danyangelofsa@gmail.com>
2023-09-27 02:32:48 -0300

    Separating responsibilities: migrating DCX-exclusive operations

---
+++ compressor/utils.cpp
@@ -0,0 +1,85 @@
+#include "utils.hpp"
+#include "compressor.hpp"
+#include "uarray.h"
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <math.h>
+#include "abbrevs.h"
+#include "malloc_count.h"
+#include "stack_count.h"
+
+
+using namespace std;
+
+void error(const char *msg) {
+    cout << "\x1b[31m[ERROR]\x1b[0m " << msg << endl;
+    exit(EXIT_FAILURE);
+}
+
+void isFileOpen(FILE * file, const char *msg) {
+    if(file != NULL) return;
+    error(msg);
+}
+
+int padding(i32 textSize, int coverage){
+    if(textSize > coverage && textSize % coverage != 0)
+        return coverage - (textSize % coverage);
+    else if(textSize % coverage !=0)
+        return coverage - textSize;
+    return 0;
+}
+
+void radixSort(i32 *text, i32 nTuples, i32 *tuples, i32 sigma, int coverage){
+    i32 *tupleIndexTemp = (i32*) calloc(nTuples, sizeof(i32));
+    
+    for(int i=0, j=0; i < nTuples; i++, j+=coverage)tuples[i] = j;
+    i32 *bucket =(i32*)malloc(sigma*sizeof(i32));
+
+    for(int d= coverage-1; d >=0; d--) {
+        for(int i=0; i < sigma;i++)bucket[i]=0;//TODO
+        for(int i=0; i < nTuples; i++) bucket[text[tuples[i] + d]+1]++; 
+        for(int i=1; i < sigma; i++) bucket[i] += bucket[i-1];
+
+        for(int i=0; i < nTuples; i++) {
+            int index = bucket[text[tuples[i] + d]]++;
+            tupleIndexTemp[index] = tuples[i];
+        }
+        for(int i=0; i < nTuples; i++) tuples[i] = tupleIndexTemp[i];
+    }
+
+    free(bucket);
+    free(tupleIndexTemp);
+}
+
+void createLexNames(i32 *text, i32 *tuples, i32 *rank, i32 &qtyRules, long int nTuples, int coverage) {
+    i32 name = 1;
+    rank[tuples[0]/coverage] = name;
+
+    for(i32 i=1; i < nTuples; i++) {
+        bool equal = true;
+        for(int j=0; j < coverage; j++){
+            if(text[tuples[i-1]+j] != text[tuples[i]+j]){
+                equal = false;
+                break;
+            }
+        }
+        if(equal) rank[tuples[i]/coverage] = name;
+        else rank[tuples[i]/coverage] = ++name;
+    }
+
+    qtyRules = name;
+    #if DEBUG == 1
+        printf("## Número de trincas %d, quantidade de trincas únicas: %d.\n", nTuples, qtyRules);
+    #endif
+}
+
+void generateReport(char *fileName, double duration, void *base) {
+    FILE *file = fopen(fileName, "a");
+    isFileOpen(file, "Unable to open report to enter time and memory consumption information");
+
+    long long int peak = malloc_count_peak();
+    long long int stack = stack_count_usage(base);
+    fprintf(file, "%lld|%lld|%5.4lf|", peak,stack,duration);
+    fclose(file);
+}
---
+++ compressor/utils.hpp
@@ -0,0 +1,73 @@
+#ifndef UTILS_COMPRESSOR_H
+#define UTILS_COMPRESSOR_H
+
+#include <iostream>
+#include <cstdint>
+#include "abbrevs.h"
+#include "uarray.h"
+
+using namespace std;
+
+template <typename T>
+void print(T v[], int n, const char *msg){
+    printf("\n%s", msg);
+    cout << *(v);
+    for(int i=1; i < n ; i++) cout  << ", " << *(v+i);
+     cout << endl;
+} 
+/**
+ * @brief Recebe uma mensagem de erro, a exibe e encerra o programa.
+ * 
+ * @param msg mensagem de erro.
+ */
+void error(const char *msg);
+
+/**
+ * @brief Verifica se FILE é diferente de NULL, em caso negativo chama a função error passando uma mensagem como parâmetro.
+ * 
+ * @param file arquivo a ser verificado.
+ * @param msg mensagem de erro, contendo o contexto do uso do arquivo.
+ */
+void isFileOpen(FILE * file, const char *msg);
+
+/**
+ * @brief Calcula a quantidade de sentinelas ($) que devem ser anexadas ao final do texto plano (ou texto reduzido)
+ * 
+ * @param textSize tamanho do texto
+ * @return quantidade de $ que devem ser anexadas ao final do texto
+ * @param coverage tamanho das tuplas do texto
+ */
+int padding(i32 textSize, int coverage);
+
+/**
+ * @brief ordena o texto com base em tuplas, iniciadas em números múltiplos de `coverage`
+ * 
+ * @param text texto a ser ordenado
+ * @param nTuples quantidade de tuplas que devem ser ordenadas
+ * @param tuples array que ao final do método conterá os índices das tuplas de forma ordenada
+ * @param sigma tamanho do alfabeto
+ * @param coverage tamanho das tuplas do texto
+ */
+void radixSort(i32 *text, i32 nTuples, i32 *tuples, i32 sigma, int coverage); 
+
+/**
+ * @brief cria lex-names para cada tupla ordenada do texto.
+ * 
+ * @param text texto a ser codificado
+ * @param tuples tuplas que foram ordenadas anteriormente pelo radix-sort
+ * @param rank array que ao final do método armazenará todos os lex-names de cada tuplas, ou seja o TEXTO REDUZIDO
+ * @param qtyRules armazena o número de tuplas sem repetição (ou seja número de regras únicas)
+ * @param nTuples quantidade de tuplas
+ * @param coverage tamanho das tuplas do texto
+ */
+void createLexNames(i32 *text, i32 *tuples, i32 *rank, i32 &qtyRules, long int nTuples, int coverage);
+
+/**
+ * @brief insere uma linha contendo informações de consumo de tempo e memória em um relatório csv
+ * 
+ * @param fileName nome do arquivo onde o relatório deve ser gravado
+ * @param duration duração em segundos da operação
+ * @param base consumo de memória
+ */
+void generateReport(char *fileName, double duration, void *base);
+#endif
