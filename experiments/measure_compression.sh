9d66c11a7ce0ad5546ae0e9f97567c8d5d5147c2
Danyelle Angelo <danyangelofsa@gmail.com>
2024-04-11 21:14:30 -0300

    graphs

--- experiments/measure_compression.sh
+++ experiments/measure_compression.sh
@@ -120,7 +120,6 @@ run_extract() {
 
 generate_graphs() {
     echo -e "\n\n${GREEN}%%% Starting the generation of the graphs. ${RESET}"
-    CURR_DATE="2024-04-10"
     python3 ../scripts/graphs/report.py "$REPORT_DIR/$CURR_DATE/*-gcx-encoding" "$REPORT_DIR/$CURR_DATE/graphs" "compress" "en"
     #python3 ../scripts/graphs/report.py "$REPORT_DIR/$CURR_DATE/*-gcx-extract" "$REPORT_DIR/$CURR_DATE/graphs" "extract" "en"
     echo -e "\n\n${GREEN}%%% FINISHED. ${RESET}"
