84290071cf122c1e194ff78daf7d53d083c2082d
Danyelle Angelo <danyangelofsa@gmail.com>
2024-04-22 19:27:49 -0300

    reports

--- experiments/measure_compression.sh
+++ experiments/measure_compression.sh
@@ -147,9 +147,9 @@ generate_graphs() {
 }
 
 if [ "$0" = "$BASH_SOURCE" ]; then
-	#check_and_create_folder
-	#download_files
+	check_and_create_folder
+	download_files
 	#compress_and_decompress_with_gcx()
-	#run_extract
-	generate_graphs
+	run_extract
+	#generate_graphs
 fi
