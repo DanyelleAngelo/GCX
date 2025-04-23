92cf89494feeccfb4f9aafdf35bffb31df0cf33d
Danyelle Angelo <danyangelofsa@gmail.com>
2025-04-22 22:34:02 -0300

    add script for compilation

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+YELLOW='\033[33m'
+GREEN='\033[0;32m'
+BLUE='\033[34m'
+RED='\033[31m'
+RESET='\033[0m'
+
+
+build_gc_star() {
+  echo "$(YELLOW)Compiling GC*...$(RESET)"
+  make compile -C gc_/ OUTPUT="$(pwd)/main_gc_"
+  make clean -C gcx/ OUTPUT="$(pwd)/main_gc_"
+  if [ $? -ne 0 ]; then
+    echo "$(RED)Error compiling GC* $(RESET)"
+    exit 1
+  fi
+  echo "$(GREEN)GC* compiled successfully as gc_star_executable.$(RESET)"
+}
+
+build_gcx() {
+  echo "$(YELLOW)Compiling GCX...$(RESET)"
+  make compile -C gcx/ OUTPUT="$(pwd)/main_gcx"
+  make clean -C gc_/ OUTPUT="$(pwd)/main_gcx"
+  if [ $? -ne 0 ]; then
+    echo "$(RED)Error compiling GCX $(RESET)"
+    exit 1
+  fi
+  echo "$(GREEN) GCX compiled successfully as gcx_executable. $(RESET)"
+}
+
+if [ "$1" == "gc_star" ]; then
+  build_gc_star
+elif [ "$1" == "gcx" ]; then
+  build_gcx
+elif [ "$1" == "all" ]; then
+  build_gc_star
+  build_gcx
+else
+  echo "Usage: $0 {gc_star|gcx|all}"
+  exit 1
+fi
